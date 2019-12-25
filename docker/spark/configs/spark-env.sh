c9dd4ec6f107446616c32f6753fce7a92d8e3788
Đặng Minh Dũng <dungdm93@live.com>
2019-12-25 19:26:39 +0700

    add default spark/conf

---
+++ docker/spark/configs/spark-env.sh
@@ -0,0 +1,7 @@
+JAVA_HOME="$(readlink -f /usr/bin/java | sed "s:/bin/java::")"
+
+HADOOP_CONF_DIR="${HADOOP_CONF_DIR:-$HADOOP_HOME/etc/hadoop}"
+YARN_CONF_DIR="${YARN_CONF_DIR:-$HADOOP_HOME/etc/hadoop}"
+HDFS_CONF_DIR="${HDFS_CONF_DIR:-$HADOOP_HOME/etc/hadoop}"
+
+SPARK_DIST_CLASSPATH="$(${HADOOP_HOME}/bin/hadoop classpath)"
