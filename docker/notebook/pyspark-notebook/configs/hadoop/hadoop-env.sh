0bb3c5d43d2c9c37ad79f52186114b7bf1093901
Đặng Minh Dũng <dungdm93@live.com>
2020-01-01 12:08:57 +0700

    update pyspark-notebook

--- docker/notebook/pyspark-notebook/configs/hadoop/hadoop-env.sh
+++ docker/notebook/pyspark-notebook/configs/hadoop/hadoop-env.sh
@@ -1,3 +1,4 @@
-export HADOOP_OS_TYPE=${HADOOP_OS_TYPE:-$(uname -s)}
+export JAVA_HOME="${JAVA_HOME:-$(readlink -f "$(which java)" | sed "s:/bin/java::")}"
+export HADOOP_OS_TYPE="${HADOOP_OS_TYPE:-$(uname -s)}"
 
 export HADOOP_CLASSPATH="${HADOOP_HOME}/share/hadoop/tools/lib/*"
--- docker/notebook/pyspark-notebook/configs/spark/spark-env.sh
+++ docker/notebook/pyspark-notebook/configs/spark/spark-env.sh
@@ -1,6 +1,6 @@
-JAVA_HOME="$(readlink -f /usr/bin/java | sed "s:/bin/java::")"
+JAVA_HOME="${JAVA_HOME:-$(readlink -f "$(which java)" | sed "s:/bin/java::")}"
 
-HADOOP_HOME="/opt/hadoop"
+HADOOP_HOME="${HADOOP_HOME:-/opt/hadoop}"
 HADOOP_CONF_DIR="${HADOOP_CONF_DIR:-$HADOOP_HOME/etc/hadoop}"
 YARN_CONF_DIR="${YARN_CONF_DIR:-$HADOOP_HOME/etc/hadoop}"
 HDFS_CONF_DIR="${HDFS_CONF_DIR:-$HADOOP_HOME/etc/hadoop}"
