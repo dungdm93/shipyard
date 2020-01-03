3467e24fa4c006b5d3d0bacc94580402e8ad5d8f
Đặng Minh Dũng <dungdm93@live.com>
2020-01-03 17:58:35 +0700

    add: Hadoop image

---
+++ docker/hadoop/configs/hadoop-env.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME="${JAVA_HOME:-$(readlink -f "$(which java)" | sed "s:/bin/java::")}"
+
+export HADOOP_OS_TYPE="${HADOOP_OS_TYPE:-$(uname -s)}"
+export HADOOP_CLASSPATH="${HADOOP_HOME}/share/hadoop/tools/lib/*"
