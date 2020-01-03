9449e2d116bb6f17bbbe982c2a8293054149dd73
Đặng Minh Dũng <dungdm93@live.com>
2020-01-03 18:57:36 +0700

    Updated: Spark image

--- docker/spark/configs/spark-env.sh
+++ docker/spark/configs/spark-env.sh
@@ -1,10 +1,1 @@
-JAVA_HOME="$(readlink -f /usr/bin/java | sed "s:/bin/java::")"
-
-HADOOP_CONF_DIR="${HADOOP_CONF_DIR:-$HADOOP_HOME/etc/hadoop}"
-YARN_CONF_DIR="${YARN_CONF_DIR:-$HADOOP_HOME/etc/hadoop}"
-HDFS_CONF_DIR="${HDFS_CONF_DIR:-$HADOOP_HOME/etc/hadoop}"
-
-SPARK_DIST_CLASSPATH="$(${HADOOP_HOME}/bin/hadoop classpath)"
-if [ ! -z "${SPARK_EXTRA_CLASSPATH}" ]; then
-    SPARK_DIST_CLASSPATH="${SPARK_DIST_CLASSPATH}:${SPARK_EXTRA_CLASSPATH}"
-fi
+JAVA_HOME="${JAVA_HOME:-$(readlink -f "$(which java)" | sed "s:/bin/java::")}"
