48cf3756d2c91824607c46304cebae40e4099349
Đặng Minh Dũng <dungdm93@live.com>
2020-01-03 15:42:50 +0700

    Add: Jupyter config to pyspark-notebook image

---
+++ docker/notebook/pyspark-notebook/configs/jupyter/jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+# Configuration file for jupyterlab.
+# Generate configs via:
+#   jupyter lab --generate-config
+# Common Directories and File Locations
+#   https://jupyter.readthedocs.io/en/latest/projects/jupyter-directories.html
+
+c.ContentsManager.allow_hidden = True
