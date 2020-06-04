2a5376d5cb932af2c3d9e61c517404d69972e6cc
Đặng Minh Dũng <dungdm93@live.com>
2020-06-04 11:52:42 +0700

    [docker/backups] update backup script

--- docker/backups/go-cron/scripts/backup_postgres.sh
+++ docker/backups/go-cron/scripts/backup_postgres.sh
@@ -5,32 +5,30 @@ if [ -z "$BACKUP_LOCATION" ]; then
   exit 1
 fi
 
-DBS=(
-)
+if [ -z "$POSTGRES_DATABASES" ]; then
+  echo >&2 "POSTGRES_DATABASES (comma separated) envvar is required"
+  exit 1
+fi
 
 function backup() {
   local dbname=$1
   local time=$(date --iso-8601=seconds)
 
-  ${POSTGRES_USERNAME:=postgres}
-  ${POSTGRES_HOST:=localhost}
-  ${POSTGRES_PORT:=5432}
+  : ${POSTGRES_USERNAME:=postgres}
+  : ${POSTGRES_HOST:=localhost}
+  : ${POSTGRES_PORT:=5432}
   PG_OPTS=(
     --host="$POSTGRES_HOST"
     --port="$POSTGRES_PORT"
     --username="$POSTGRES_USERNAME"
   )
-  if [ ! -z "$POSTGRES_PASSWORD" ]; then
-    PG_OPTS+=(
-        --password="$POSTGRES_PASSWORD"
-    )
-  fi
 
   echo "backup db=$dbname time=$time"
-  pg_dump "${PG_OPTS[@]}" --dbname=$dbname --compress=9 |
+  PGPASSWORD="$POSTGRES_PASSWORD" pg_dump "${PG_OPTS[@]}" --dbname=$dbname --compress=9 |
   mc pipe "$BACKUP_LOCATION/$dbname/$time.gz";
 }
 
+IFS=',' read -ra DBS <<< "$POSTGRES_DATABASES"
 for db in "${DBS[@]}"; do
   backup $db
 done
