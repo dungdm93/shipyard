835a22f3d9d71e99515b20c125f2ad19e92adb76
Đặng Minh Dũng <dungdm93@live.com>
2020-07-08 10:47:53 +0700

    [docker/samples] pod-info v2

--- docker/samples/pod-info/main.py
+++ docker/samples/pod-info/main.py
@@ -1,24 +1,30 @@
 import json
+import logging
 import os
 import socket
+import sys
 
 from flask import Flask
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger(__file__)
+
 app = Flask("pod-info")
+
+info = json.dumps({
+    "hostname": socket.gethostname(),
+    "namespace": os.getenv("MY_POD_NAMESPACE", "unknown"),
+    "pod_name": os.getenv("MY_POD_NAME", "unknown"),
+    "pod_ip": os.getenv("MY_POD_IP", "unknown"),
+    "node_name": os.getenv("MY_NODE_NAME", "unknown"),
+    "service_account": os.getenv("MY_POD_SERVICE_ACCOUNT", "unknown")
+})
 
 
 @app.route('/')
 def index():
-    info = {
-        "hostname": socket.gethostname(),
-        "namespace": os.getenv("MY_POD_NAMESPACE", "unknown"),
-        "pod_name": os.getenv("MY_POD_NAME", "unknown"),
-        "pod_ip": os.getenv("MY_POD_IP", "unknown"),
-        "node_name": os.getenv("MY_NODE_NAME", "unknown"),
-        "service_account": os.getenv("MY_POD_SERVICE_ACCOUNT", "unknown")
-    }
-
-    return json.dumps(info) + "\n"
+    logger.info(info)
+    return info + "\n"
 
 
 if __name__ == '__main__':
