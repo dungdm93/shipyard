1c42896ceca540d2c721eb89fc9348369b4c5ea3
Liam <luyen.nv@teko.vn>
2021-05-18 14:18:58 +0700

    [docker/superset] Add Trino OAuth 2

--- docker/superset/trino.py
+++ docker/superset/trino.py
@@ -1,11 +1,69 @@
 from datetime import datetime
-from typing import Optional
+from typing import Any, Dict, Optional, TYPE_CHECKING
 from urllib import parse
 
 from sqlalchemy.engine.url import URL
+from trino.auth import Authentication
 
 from superset.db_engine_specs.base import BaseEngineSpec
 from superset.utils import core as utils
+from superset.extensions import cache_manager
+
+if TYPE_CHECKING:
+    from superset.models.core import Database
+
+token_cache_key = "TOKEN_OAUTH2_TOKEN_KEY"
+
+
+class OAuth2Authentication(Authentication):
+    def __init__(
+        self,
+        client_id: str,
+        client_secret: str,
+        token_endpoint: str,
+    ) -> None:
+        from authlib.integrations.requests_client import OAuth2Session
+
+        self.client = OAuth2Session(
+            client_id=client_id,
+            client_secret=client_secret,
+            token_endpoint=token_endpoint,
+        )
+        self.client.update_token = OAuth2Authentication.update_token(self.client)
+
+    def set_client_session(self, client_session):
+        pass
+
+    def set_http_session(self, http_session):
+        if not self.client.token:
+            self.initialize_token(self.client)
+        http_session.auth = self.client.token_auth
+        return http_session
+
+    def setup(self, trino_client):
+        self.set_client_session(trino_client.client_session)
+        self.set_http_session(trino_client.http_session)
+
+    def get_exceptions(self):
+        return ()
+
+    def handle_error(self, handle_error):
+        pass
+
+    @staticmethod
+    def update_token(client):
+        def func(token, refresh_token=None, access_token=None):
+            client.token = token
+
+        return func
+
+    @staticmethod
+    def initialize_token(client):
+        token = cache_manager.cache.get(token_cache_key)
+        if token is None:
+            token = client.fetch_token(grant_type='client_credentials')
+            cache_manager.cache.set(key=token_cache_key, value=token, timeout=600)
+        client.update_token(token)
 
 
 class TrinoEngineSpec(BaseEngineSpec):
@@ -36,7 +94,7 @@ class TrinoEngineSpec(BaseEngineSpec):
             value = dttm.date().isoformat()
             return f"from_iso8601_date('{value}')"
         if tt == utils.TemporalType.TIMESTAMP:
-            value = dttm.isoformat(timespec='microseconds')
+            value = dttm.isoformat(timespec="microseconds")
             return f"from_iso8601_timestamp('{value}')"
         return None
 
@@ -53,3 +111,35 @@ class TrinoEngineSpec(BaseEngineSpec):
             selected_schema = parse.quote(selected_schema, safe="")
             database = database.split("/")[0] + "/" + selected_schema
             uri.database = database
+
+    @staticmethod
+    def get_extra_params(database: "Database") -> Dict[str, Any]:
+        extra: Dict[str, Any] = BaseEngineSpec.get_extra_params(database)
+        engine_params: Dict[str, Any] = extra.setdefault("engine_params", {})
+        connect_args: Dict[str, Any] = engine_params.setdefault("connect_args", {})
+
+        if database.server_cert:
+            connect_args["http_scheme"] = "https"
+            connect_args["verify"] = utils.create_ssl_cert_file(database.server_cert)
+
+        return extra
+
+    @staticmethod
+    def get_encrypted_extra_params(database: "Database") -> Dict[str, Any]:
+        extra: Dict[str, Any] = BaseEngineSpec.get_encrypted_extra_params(database)
+
+        auth_method = extra.pop("auth_method", None)
+        auth_params = extra.pop("auth_params", {})
+        if not auth_method:
+            return extra
+
+        connect_args = extra.setdefault("connect_args", {})
+        connect_args["http_scheme"] = "https"
+        if auth_method == "kerberos":
+            from trino.auth import KerberosAuthentication
+            connect_args["auth"] = KerberosAuthentication(**auth_params)
+        elif auth_method == "oauth2_client_credentials":
+            auth = OAuth2Authentication(**auth_params)
+            print(id(auth))
+            connect_args["auth"] = auth
+        return extra
