4847c966bbf36197e548e0cc941431aba95a8af0
Đặng Minh Dũng <dungdm93@live.com>
2021-08-07 18:47:03 +0700

    [docker/tools/terraform] upgrade terraform to 1.0

--- docker/tools/terraform/providers.tf
+++ docker/tools/terraform/providers.tf
@@ -2,62 +2,62 @@ terraform {
   required_providers {
     ## CNCF Kubernetes
     kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.4"
     }
 
     ## CNCF Helm
     helm = {
-      source = "hashicorp/helm"
-      version = "~> 2.0"
+      source  = "hashicorp/helm"
+      version = "~> 2.2"
     }
 
     ## Amazon AWS
     # aws = {
     #   source  = "hashicorp/aws"
-    #   version = "~> 2.70"
+    #   version = "~> 3.50"
     # }
 
     ## Google Cloud Platform
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.50"
+      version = "~> 3.75"
     }
 
     ## Microsoft Azure
     # azurerm = {
     #   source  = "hashicorp/azurerm"
-    #   version = "~> 2.40"
+    #   version = "~> 2.70"
     # }
 
     ## Oracle Cloud Infrastructure
     # oci = {
     #   source  = "hashicorp/oci"
-    #   version = "~> 4.10"
+    #   version = "~> 4.38"
     # }
 
     ## OpenStack
     # openstack = {
     #   source  = "terraform-provider-openstack/openstack"
-    #   version = "~> 1.30"
+    #   version = "~> 1.40"
     # }
 
     ## VMware vSphere
     # vsphere = {
     #   source  = "hashicorp/vsphere"
-    #   version = "~> 1.20"
+    #   version = "~> 2.0"
     # }
 
     ## Alibaba Cloud
     # alicloud = {
     #   source  = "aliyun/alicloud"
-    #   version = "~> 1.100"
+    #   version = "~> 1.128"
     # }
 
     ## Cloudflare
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 2.15"
+      version = "~> 2.25"
     }
 
     ## Database PostgreSQL
@@ -68,8 +68,8 @@ terraform {
 
     ## Database MySQL
     mysql = {
-      source  = "petoju/mysql"
-      version = "~> 2.2"
+      source  = "winebarrel/mysql"
+      version = "~> 1.10"
     }
 
     ## RabbitMQ
@@ -81,7 +81,13 @@ terraform {
     ## Hashicorp Vault
     vault = {
       source  = "hashicorp/vault"
-      version = "~> 2.18"
+      version = "~> 2.22"
+    }
+
+    # Grafana
+    grafana = {
+      source  = "grafana/grafana"
+      version = "~> 1.13"
     }
 
     ## MinIO
@@ -99,7 +105,7 @@ terraform {
     ## Keycloak
     keycloak = {
       source  = "mrparkers/keycloak"
-      version = "2.2"
+      version = "3.2"
     }
   }
 }
