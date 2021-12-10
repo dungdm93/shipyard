522e4077cffcfc974d80c28a21a3510351145d5a
Đặng Minh Dũng <dungdm93@live.com>
2021-12-10 10:21:45 +0700

    [docker/jupyter/pyspark-notebook] upgrade pyspark to 3.2.0

---
+++ docker/jupyter/pyspark-notebook/configs/conda/etc/ipython/ipython_kernel_config.py
@@ -0,0 +1,4 @@
+# The IPython directory
+#   https://ipython.readthedocs.io/en/stable/config/intro.html#systemwide-configuration
+
+c.InteractiveShellApp.extensions.append('sparkmonitor.kernelextension')
