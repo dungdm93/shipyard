6e99f8968f5146eec4ec44f6c5395aacda388786
Đặng Minh Dũng <dungdm93@live.com>
2022-06-03 16:51:04 +0700

    [docker/jupyter/pyspark-notebook] * update base image * remove unused python driver * remove sparkmonitor

--- docker/jupyter/pyspark-notebook/configs/conda/etc/ipython/ipython_kernel_config.py
+++ docker/jupyter/pyspark-notebook/configs/conda/etc/ipython/ipython_kernel_config.py
@@ -1,4 +1,4 @@
 # The IPython directory
 #   https://ipython.readthedocs.io/en/stable/config/intro.html#systemwide-configuration
 
-c.InteractiveShellApp.extensions.append('sparkmonitor.kernelextension')
+# c.InteractiveShellApp.extensions.append('sparkmonitor.kernelextension')
